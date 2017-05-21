469b0f6fab85edfa7ee8a914f5f4ec106b701000
vilhena32 <joana.vilhena.32@gmail.com>
2017-05-21 18:12:54 +0100

    //Pesquisa Resultados

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
 use App\Department;
 
 class DepartmentController extends Controller
@@ -17,8 +19,12 @@ class DepartmentController extends Controller
     public function listDepartments()
     {
         $title = 'List departments';
+
+        $departments = Department::all(); 
         $departments = Department::paginate(5);
 
         return view('departments.index', compact('title', 'departments'));
     }
+
 }
+;
